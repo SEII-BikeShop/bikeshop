e8e411145aca3ae9f0f9983f7a01066ccd194d68
Aaron Barlow <abarlow505@gmail.com>
2020-02-08 19:49:30 -0500

    Setup agnostic database configuration files for team members

--- configs/settings.py
+++ configs/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'bikeshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# The config.py file
+from .config import *
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bikeshop',
+        'USER': USER,
+        'PASSWORD': PASS
     }
 }
 
