2d5460872c9965ad655158cdaac1232b0dc6364b
Aaron Barlow <abarlow505@gmail.com>
2020-02-10 15:51:53 -0500

    Add two SE requirements

---
+++ SEII-Entities/Bike.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeShopAnalyticsAPI.Models.Entities
+{
+    public class Bike
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BikeID { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal SalesPrice { get; set; }
+
+        /*  To be added later.
+        public ModelType Type { get; set; }
+        public Color Color { get; set; }
+        */
+    }
+}
---
+++ SEII-Entities/SalesOrder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeShopAnalyticsAPI.Models.Entities
+{
+    public class SalesOrder
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SalesID { get; set; }
+
+        [ForeignKey("Bike")]
+        public int BikeID { get; set; }
+
+        public decimal SalesPrice { get; set; }
+
+        public decimal Tax { get; set; }
+
+        public decimal Discount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string State { get; set; }
+
+        public int Units { get; set; }
+
+
+        /*  To be added later.
+        [ForeignKey("Customer")]
+        public int CustomerID { get; set; }
+
+        [ForeignKey("Employee")]
+        public int EmployeeID { get; set; }
+        
+        [ForeignKey("Store")]
+        public int StoreID { get; set; }
+
+        */
+
+        public virtual Bike Bike { get; set; }
+        
+        /*  TO be added later.
+        public virtual Customer Customer { get; set; }
+        public virtual Employee Employee { get; set; }
+        public virtual Store Store { get; set; }
+        */
+
+
+    }
+}
