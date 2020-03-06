92f33bd6c6b99d975bb9092e999de81737c5b200
Aaron Barlow <abarlow505@gmail.com>
2020-03-06 18:16:11 -0500

    Fix logout

--- web/web/urls.py
+++ web/web/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('index', views.index, name='index'),
     path('login', views.login, name='login'),
-    path('logout', views.login, name='logout')
+    path('logout', views.logout, name='logout')
 ]
