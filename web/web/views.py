a4cdce7bfcb6029691079cb11c7d17cc35d88f89
Aaron Barlow <abarlow505@gmail.com>
2020-03-06 15:22:06 -0500

    Add login post request to API

--- web/web/views.py
+++ web/web/views.py
@@ -1,8 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from .forms import LoginForm
+import requests
 
 def index(request):
     return render(request, 'index.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+            # create a form instance and populate it with data from the request:
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                # print(form.cleaned_data['username'])
+
+                url = 'http://127.0.0.1:8080/api/v0/city/?format=json'
+                filter = {'key': 'value'}
+
+                data = requests.get(url, data = filter)
+
+                print(data.text)
+
+
+                return HttpResponseRedirect('')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
