ee8c7525c38b8491d23a04886a854444a9ee5856
alee3960 <alee3960@gmail.com>
2020-03-11 05:54:03 -0400

    added add_bike and create_admin

--- web/web/urls.py
+++ web/web/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('delete_bike/<int:id>/', views.delete_bike, name='delete_bike'),
     path('add_bike', views.add_bike, name='add_bike'),
-    path('create_admin', views.create_admin, name='create_admin')
+    path('create_admin', views.create_admin, name='create_admin'),
+    path('update_bike/<int:id>/', views.update_bike, name='update_bike')
 ]
--- web/web/views.py
+++ web/web/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm, SearchForm, EditForm
 import requests
 
 from django.core import serializers
@@ -87,8 +87,77 @@ def delete_bike(request, id):
 
     return HttpResponseRedirect('/index')
 
+def update_bike(request, id):
+    if request.method == 'POST':
+            # create a form instance and populate it with data from the request:
+            form = EditForm(request.POST)
+            if form.is_valid():
+                try:
+                    url = 'http://127.0.0.1:8080/api/v0/bicycle/{}/'.format(id)
+                    data = form.cleaned_data
+                    result = requests.patch(url, data = data)
+
+                    print(result)
+
+                    return HttpResponseRedirect('/index')
+                except Exception:
+                    print('NO USER')
+                    return render(request, 'edit.html', {'form': form, 'error': 'ERROR: Data is invalid'})
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        url = 'http://127.0.0.1:8080/api/v0/bicycle/{}?format=json'.format(id)
+        data = requests.get(url)
+        parsed_data = json.loads(data.text)
+        form = EditForm(initial=parsed_data)
+
+    return render(request, 'edit.html', {'form': form})
+
 def add_bike(request):
-    return render(request, 'add_bike.html')
+    # create a form instance and populate it with data from the request:
+    form = BicycleForm(request.POST)
+    if form.is_valid():
+        try:
+            bike = form.cleaned_data
+            bike.save()
+            print(bike)
+            return HttpResponseRedirect('/index')
+        except Exception:
+            print('Adding error')
+            return render(request, 'add_bike.html', {'form': form, 'error': 'ERROR: Data is invalid'})
+
+    return render(request, 'add_bike.html', {'form': form})
+
+    # if a GET (or any other method) we'll create a blank form
+    # else:
+    #     url = 'http://127.0.0.1:8080/api/v0/bicycle/{}?format=json'.format(id)
+    #     data = requests.get(url)
+    #     parsed_data = json.loads(data.text)
+    #     form = EditForm(initial=parsed_data)
+
+    # f = CreateForm(request.POST)
+    # bike = f.cleaned_data
+    # bike.save()
+
+
+    # if request.method == 'POST':
+    #     form = CreateForm(request.POST)
+    #     if form.is_valid():
+    #         modeltype = form.cleaned_data['modeltype']
+    #         listprice = form.cleaned_data['listprice']
+    #         construction = form.cleaned_data['construction']
+    #         letterstyleid = form.cleaned_data['letterstyleid']
+    #         framesize = form.cleaned_data['framesize']
+    #         toptube = form.cleaned_data['toptube']
+    #         chainstay = form.cleaned_data['chainstay']
+    #         headtubeangle = form.cleaned_data['headtubeangle']
+    #         seattubeangle = form.cleaned_data['seattubeangle']
+    #         query = Bicycle(modeltype = modeltype, listprice = listprice,
+    #             construction = construction, letterstyleid = letterstyleid,
+    #             framesize = framesize, toptube = toptube,
+    #             chainstay = chainstay, headtubeangle = headtubeangle,
+    #             seattubeangle = seattubeangle)
+    #         query.save()
+    # return render(request, 'add_bike.html', {'form': form})
 
 def create_admin(request):
     admin_info = {
