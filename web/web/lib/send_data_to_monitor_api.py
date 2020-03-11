89a2fb68bb3f7a924dbcb5ccb0f7cfd4d2ea836c
Aaron Barlow <abarlow505@gmail.com>
2020-03-11 12:33:23 -0400

    Add ability to create account

---
+++ web/web/lib/send_data_to_monitor_api.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+USERNAME = 'team_db'
+PASSWORD = 'password'
+TOKEN = '123d46bbe98753eb707ed6dc66e97979'
+
+# If you don't already have an account
+def create_account():
+    url = 'http://bikeshopmonitoring.duckdns.org/Admin/Create'.format(id)
+    data = {
+        "Admin": {
+            "FirstName": 'First',
+            "LastName": 'Last',
+            "MiddleName": 'Middle',
+            "Email": 'db_team@example.com',
+            "UserName": USERNAME
+        },
+        "Password": PASSWORD
+    }
+    data = requests.post(url, data = data, verify=False, headers={"Content-Type":"application/json"})
+    print(data)
+# create_account()
