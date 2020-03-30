38280907ebfbf4c684b71afe531bfe101030d48a
Aaron Barlow <abarlow505@gmail.com>
2020-03-30 17:13:15 -0400

    Create bicycle test

--- api/api/tests.py
+++ api/api/tests.py
@@ -5,26 +5,6 @@ from api.models import *
 from dateutil import parser
 import csv
 
-class EndpointTestCase(APITestCase):
-
-    # def setup(self):
-    #     import_test_data = ImportTestData()
-    #     import_test_data.import_data(max_records_per_table = 5)
-
-    def test_get_user(self):
-        Users.objects.get_or_create(
-            id=1, username='customer', usertype='customer', usertypeid=1
-        )
-
-        data = {}
-        url = 'http://127.0.0.1:8080/api/v0/users/1/?format=json'
-        response = self.client.get(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-
-
-
-
 class ImportTestData:
 
     def __init__(self):
@@ -44,8 +24,9 @@ class ImportTestData:
         except ValueError:
             return False
 
-    def import_data(self, max_records_per_table=5):
+    def import_data(self, max_records_per_table):
         for table in self.tables:
+
             if table.objects.count() > max_records_per_table:
                 break
 
@@ -92,10 +73,41 @@ class ImportTestData:
                     try:
                         table.objects.get_or_create(**record)
                     except Exception as error:
-                        if 'Duplicate entry' not in str(error):
-                            print('Table {}'.format(table.__name__))
-                            print('Error: ', error)
-                            print('Record data: ', record)
-                            print('\n')
-                            print('\n')
                         pass
+
+
+
+
+class EndpointTestCase(APITestCase):
+
+    def setUp(self, *args):
+        import_test_data = ImportTestData()
+        import_test_data.import_data(max_records_per_table = 1)
+
+    def test_get_user(self):
+        Users.objects.get_or_create(
+            id=1, username='customer', usertype='customer', usertypeid=1
+        )
+
+        data = {}
+        url = 'http://127.0.0.1:8080/api/v0/users/1/'
+        response = self.client.get(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_bicycle(self):
+
+        Bicycle.objects.get_or_create(
+            serialnumber=1, customerid=Customer.objects.first(), modeltype=Modeltype.objects.first(),
+            paintid=Paint.objects.first(), framesize=23, orderdate=parser.parse('11/6/1994'),
+            startdate=parser.parse('11/6/1994'), shipdate=parser.parse('11/6/1994'),
+            shipemployee=1, frameassembler=1, painter=1, construction='Bonded',
+            waterbottlebrazeons=4, customname='Roman', storeid=Retailstore.objects.first(),
+            employeeid=Employee.objects.first(), toptube=1, chainstay=43, headtubeangle=32, seattubeangle=32,
+            listprice=1250, salestax=65, salestate='TN', shipprice=50,
+            frameprice=50, componentlist=50
+        )
+
+        data = {}
+        url = 'http://127.0.0.1:8080/api/v0/bicycle/1/'
+        response = self.client.get(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
