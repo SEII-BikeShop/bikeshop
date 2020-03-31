7d76351ec160366d034855918c0513d5fa2ef673
Aaron Barlow <abarlow505@gmail.com>
2020-03-31 12:12:28 -0400

    Add bicycletubeusage endpoint test

--- api/api/tests.py
+++ api/api/tests.py
@@ -2,44 +2,7 @@ from django.urls import include, path, reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, URLPatternsTestCase
 from api.models import *
-from dateutil import parser
 import requests
-import csv
-
-
-class EndpointTestCase(APITestCase):
-
-    def test_get_user(self):
-        data = {}
-        url = 'http://127.0.0.1:8080/api/v0/users/1?format=json'
-        response = requests.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_get_bicycle(self):
-        data = {}
-        url = 'http://127.0.0.1:8080/api/v0/bicycle/1?format=json'
-        response = requests.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_get_city(self):
-        data = {}
-        url = 'http://127.0.0.1:8080/api/v0/city/2?format=json'
-        response = requests.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_get_paint(self):
-        data = {}
-        url = 'http://127.0.0.1:8080/api/v0/paint/1?format=json'
-        response = requests.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-from django.urls import include, path, reverse
-from rest_framework import status
-from rest_framework.test import APITestCase, URLPatternsTestCase
-from api.models import *
-from dateutil import parser
-import requests
-import csv
 
 
 class EndpointTestCase(APITestCase):
@@ -71,5 +34,11 @@ class EndpointTestCase(APITestCase):
     def test_get_employee(self):
         data = {}
         url = 'http://127.0.0.1:8080/api/v0/employee/0?format=json'
+        response = requests.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_bicycle_tube_usage(self):
+        data = {}
+        url = 'http://127.0.0.1:8080/api/v0/bicycletubeusage/1/?format=json'
         response = requests.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
