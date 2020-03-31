c7600ee52e571a7fb695976e1d51ccbc312b82bc
Aaron Barlow <abarlow505@gmail.com>
2020-03-31 13:17:35 -0400

    Add groupo test endpoint

--- api/api/tests.py
+++ api/api/tests.py
@@ -66,3 +66,9 @@ class EndpointTestCase(APITestCase):
         url = 'http://127.0.0.1:8080/api/v0/customer/1?format=json'
         response = requests.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_groupo(self):
+        data = {}
+        url = 'http://127.0.0.1:8080/api/v0/groupo/1?format=json'
+        response = requests.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
